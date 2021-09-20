1d5b33df550970dc7b4602090473bd7286a7372d
soh92 <pix4soh@gmail.com>
2021-09-20 22:04:50 +0900

    Initial Commit

---
+++ HelloMetal/HelloMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMetalApp.swift
+//  HelloMetal
+//
+//  Created by sho yoneda on 2021/09/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloMetal/Renderer.swift
@@ -0,0 +1,20 @@
+//
+//  Renderer.swift
+//  HelloMetal
+//
+//  Created by sho yoneda on 2021/09/20.
+//
+
+import Foundation
+import MetalKit
+
+class Renderer: NSObject, MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+
+    }
+}
